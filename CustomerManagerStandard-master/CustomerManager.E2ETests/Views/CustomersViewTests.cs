784449886e33f42a38875f80abb0c426918ef8bb
robertmorris <ryanmorris793@gmail.com>
2014-02-11 10:08:17 -0500

    first commit

---
+++ CustomerManagerStandard-master/CustomerManager.E2ETests/Views/CustomersViewTests.cs
@@ -0,0 +1,186 @@
+﻿using System;
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using Protractor;
+using CustomerManager.E2ETests.PageObjects;
+
+namespace CustomerManager.E2ETests
+{
+    public class CustomersViewTests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Using NuGet Package 'PhantomJS' (Headless browser testing for build servers)
+            //driver = new OpenQA.Selenium.PhantomJS.PhantomJSDriver();
+
+            // Using NuGet Package 'WebDriver.ChromeDriver.win32'
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+
+            driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(30));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void CardViewByDefault()
+        {
+            var ngDriver = new NgWebDriver(driver);
+            ngDriver.Navigate().GoToUrl("http://localhost:58000/#/customers");
+
+            var customersPage = new CustomersPage(ngDriver);
+
+            Assert.IsTrue(customersPage.IsCardViewSelected());
+        }
+
+        [Test]
+        public void SwitchToListView()
+        {
+            var ngDriver = new NgWebDriver(driver);
+            ngDriver.Navigate().GoToUrl("http://localhost:58000/#/customers");
+
+            var customersPage = new CustomersPage(ngDriver);
+            customersPage.SwitchToListView();
+
+            Assert.IsTrue(customersPage.IsListViewSelected());
+        }
+
+        [Test]
+        public void ShowTop10Customers()
+        {
+            var ngDriver = new NgWebDriver(driver);
+            ngDriver.Navigate().GoToUrl("http://localhost:58000/#/customers");
+
+            var customersPage = new CustomersPage(ngDriver);
+            Assert.AreEqual(10, customersPage.GetCustomersCount());
+
+            IWebElement footer = ngDriver.FindElement(NgBy.Binding("totalRecords"));
+            Assert.AreEqual("Showing 10 of 23 total customers", footer.Text);
+        }
+
+        [Test]
+        public void ShouldFilter()
+        {
+            var ngDriver = new NgWebDriver(driver);
+            ngDriver.Navigate().GoToUrl("http://localhost:58000/#/customers");
+
+            var customersPage = new CustomersPage(ngDriver);
+
+            Assert.AreEqual(4, customersPage.Filter("Wahlin").GetCustomersCount()); // Fail!
+        }
+
+        [Test]
+        public void ShowCustomerOrders()
+        {
+            var ngDriver = new NgWebDriver(driver);
+            ngDriver.Navigate().GoToUrl("http://localhost:58000/#/customers");
+
+            var customerOrdersPage = new CustomersPage(ngDriver).GoToCustomerOrders(2);
+
+            Assert.AreEqual("Dan Wahlin", customerOrdersPage.GetCustomerName());
+            Assert.AreEqual(5, customerOrdersPage.GetOrdersCount());
+        }
+
+        // etc.
+
+        // Examples using mocked backend (See 'Support' folder)
+
+        [Test]
+        public void NoCustomersOnCardView()
+        {
+            NgMockE2EModule mockModule = new NgMockE2EModule(@"
+$httpBackend.whenGET(/^\/app\/partials\//).passThrough();
+
+$httpBackend.whenGET(/^\/app\/views\//).passThrough();
+
+$httpBackend.whenGET('\/api/dataservice/customersSummary?$top=10&$skip=0').respond(
+// status
+200,
+// body
+[],
+// headers
+{
+    'X-InlineCount': 0
+}
+);"
+                );
+
+            var ngDriver = new NgWebDriver(driver, mockModule);
+            ngDriver.Navigate().GoToUrl("http://localhost:58000/#/customers");
+
+            var customersPage = new CustomersPage(ngDriver);
+            Assert.AreEqual(0, customersPage.GetCustomersCount());
+            Assert.AreEqual("No customers found", customersPage.GetFooterText());
+        }
+
+        [Test]
+        public void DeleteCustomerOnCardView()
+        {
+            NgMockE2EModule mockModule = new NgMockE2EModule(@"
+$httpBackend.whenGET(/^\/app\/partials\//).passThrough();
+
+$httpBackend.whenGET(/^\/app\/views\//).passThrough();
+
+$httpBackend.whenGET('\/api/dataservice/customersSummary?$top=10&$skip=0').respond(
+// status
+200,
+// body
+[
+    { 
+        id:1, 
+        firstName:'Marcus',
+        lastName:'HighTower',
+        city:'Phoenix',
+        state: {
+            id:1,
+            abbreviation:'AZ',
+            name:' Arizona'
+        },
+        orderCount:6,
+        gender:'Male'
+    },
+    {
+        id:2,
+        firstName:'Dan',
+        lastName:'Wahlin',
+        city:'Chandler',
+        state:{
+            id:1,
+            abbreviation:'AZ',
+            name:' Arizona'
+        },
+        orderCount:5,
+        gender:'Male'
+    }
+],
+// headers
+{
+    'X-InlineCount': 2
+}
+);"
+                );
+
+            var ngDriver = new NgWebDriver(driver, mockModule);
+            ngDriver.Navigate().GoToUrl("http://localhost:58000/#/customers");
+
+            var customersPage = new CustomersPage(ngDriver);
+            Assert.AreEqual(2, customersPage.GetCustomersCount());
+            Assert.AreEqual("Showing 2 of 2 total customers", customersPage.GetFooterText());
+
+            // Delete Dan (sorry...)
+            customersPage.DeleteCustomer(2).ConfirmDelete();
+
+            Assert.AreEqual(1, customersPage.GetCustomersCount());
+            Assert.AreEqual("Showing 1 of 1 total customers", customersPage.GetFooterText()); // Fail!
+        }
+
+        // etc.
+    }
+}
---
+++ CustomerManagerStandard-master/readme.md
@@ -0,0 +1,76 @@
+﻿Customer Manager with AngularJS
+===============
+
+If you’re new to AngularJS check out my [AngularJS in 60-ish Minutes](http://weblogs.asp.net/dwahlin/archive/2013/04/12/video-tutorial-angularjs-fundamentals-in-60-ish-minutes.aspx) video tutorial or download the [free eBook](http://weblogs.asp.net/dwahlin/archive/2013/07/30/angularjs-in-60-ish-minutes-the-ebook.aspx). Also check out [The AngularJS Magazine](http://flip.it/bdyUX) for up-to-date information on using AngularJS to build Single Page Applications (SPAs).
+
+![Customer Management App](https://raw.github.com/DanWahlin/CustomerManager/master/CustomerManager/Content/images/customerApp.png)
+
+This is the standard Customer Manager application. To view a version of the application that has custom routing visit [https://github.com/DanWahlin/CustomerManager](https://github.com/DanWahlin/CustomerManager).
+This application demonstrates:
+
+* A complete application with read-only and editable data
+* Using AngularJS with $http in a factory to access a backend Web API service
+* Using BreezeJS in a factory to access a backend Web API Service
+* Techniques for showing multiple views of data (card view and list view)
+* A custom filter for filtering customer data
+* A custom directive to ensure unique values in a form for email 
+* A custom directive that intercepts $http and jQuery XHR requests (in case either are used) and displays a loading dialog
+
+The factories can be switched by changing the app/services/config useBreeze setting to true.
+
+The AngularJS portion of the app is structured using the following folders:
+
+![Customer Management App Structure](https://raw.github.com/DanWahlin/CustomerManager/master/CustomerManager/Content/images/appFolders.png)
+
+## Requirements:
+
+###If you're using Visual Studio and .NET:
+
+The following is required to support the backend services:
+
+* Visual Studio 2012 Web Express (free version) or higher - http://www.microsoft.com/visualstudio/eng/products/visual-studio-express-products#product-express-summary
+
+* ASP.NET MVC and Web API are used for the back-end services along with Entity Framework for database access (included if you have VS 2012 express installed)
+
+To get started, double-click the CustomerManager.sln file located at the root of the CustomerManager repository. Once the solution loads press F5 to run the project.
+
+
+###If you're using Node.js/Express/MongoDB
+
+If you don't already have Node.js on your machine install it from http://nodejs.org. You'll also need to install MongoDB from http://www.mongodb.org if you don't have it already and get it configured and running using the instructions on their site.
+
+In the CustomerManager directory execute 'npm install' to install Express, MongoDB and Mongoose (package.json).
+
+Load sample data into MongoDB by performing the following steps:
+
+* Execute 'mongod' to start the MongoDB daemon
+* Navigate to the CustomerManager directory (the one that has initMongoCustData.js in it) then execute 'mongo' to start the MongoDB shell
+* Enter the following in the mongo shell to load the seed files:
+ * use custmgr
+ * load("initMongoCustData.js")
+ * load("initMongoSettingsData.js")
+ * load("initMongoStateData.js")
+
+Start the Node/Express server:
+* navigate to the CustomerManager/server directory then execute 'node app.js'
+
+View the application at http://localhost:3000
+
+Thanks to [Tony Quinn](https://github.com/tonyq) for contributing the initial Node.js/MongoDB code!
+
+## E2E tests using Protractor:
+
+_E2E tests using Protractor for .NET, and Selenium's WebDriver wrapper for Angular_
+
+Build `CustomerManager.E2ETests` project to restore NuGet packages.
+
+Tests are written with [NUnit](http://nunit.org/) using the _Page Object_ [[1]](https://code.google.com/p/selenium/wiki/PageObjects)[[2]](http://martinfowler.com/bliki/PageObject.html) pattern to make your tests more readable.
+(Tests use `http://localhost:58000/` so make sure IIS Express is running the app)
+
+There are several ways to execute these tests:
+
+* Using NUnit TestAdapter for Visual Studio 2012/2013 from the [NUnitTestAdapter](https://www.nuget.org/packages/NUnitTestAdapter) NuGet package.
+* Using Visual Studio extensions like [ReSharper](http://www.jetbrains.com/resharper/) or [TestDriven.net](http://www.testdriven.net/).
+* Using the command line or GUI provided by the [NUnit.Runners](https://www.nuget.org/packages/NUnit.Runners) NuGet package.
+
+Thanks to [Bruno Baia](https://github.com/bbaia) for the contribution!
